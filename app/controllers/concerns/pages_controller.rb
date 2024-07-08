222b8bbebff409ce864dc40979f6e7d6ed82df79
Noriko Iida <lumos.5503@gmail.com>
2024-07-08 12:21:07 +0000

    puls_a

---
+++ app/controllers/concerns/pages_controller.rb
@@ -0,0 +1,4 @@
+class Public::PagesController < ApplicationController
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
 
-  root to: "homes#top"
-  devise_for :users
-  get "/homes/about" => "homes#about", as: "about"
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorite, only: [:create, :destroy]
-    resources :post_comments, only: [:create, :destroy]
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
   end
 
-  resources :users, only: [:show, :edit, :update]
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  scope module: :public do
+    devise_for :users
+    root to: 'homes#top'
+    get 'homes/about', to: 'homes#about', as: :about
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
+   end
+  end
